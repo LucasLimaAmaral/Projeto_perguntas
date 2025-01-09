871bd57398d954b89ae07fa987575c1311160133
Lucas Amaral <amaralx360@gmail.com>
2025-01-09 16:08:19 -0300

    foi criado uma lista de perguntas, uma coluna com uma lista de widgets e os widgets são text(perguntas) e um EleveteButton(resposta)

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(PerguntaApp());
+main() => runApp(PerguntaAppKey());
 
-class PerguntaApp extends StatelessWidget{
+class PerguntaAppKey extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+
+    ]; 
+
     return MaterialApp(
       home: Scaffold(
+
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola, Flutter!!'),
+
+        body: Column(
+
+          children: <Widget>[
+
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1')
+            )
+
+          ],
+
+        ),
+
       ),
     );
   }
 }
-//sdfsdf
